973d24f06af9de6388d8ac000a3c617268473a4d
Nathan Le Roux <152197824+NathanLeRoux29@users.noreply.github.com>
2024-04-10 11:11:24 +0200

    On voit des trucs

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 if (!defined('ROOT_PATH')) {
     define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
 }
--- templates/index.php
+++ templates/index.php
@@ -1,5 +1,8 @@
 <?php
 
+
+require_once ('./functions/router.php');   //<---------- Nathan: Pour Les fonction get-server et register_route
+
 if (!session_id()) session_start();
 try {
     $request_uri = get_server('request_uri');
@@ -17,9 +20,9 @@ try {
         '/api/post',
     ));
 
-    if($request_uri == "/") {
+    // if($request_uri == "/") {
         $request_uri = '/home';
-    }
+    // }
 
     $requested_route = formate_route($request_uri);
 
