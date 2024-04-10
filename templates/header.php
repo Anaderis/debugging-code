21f0c4c921432fc073e85de9eb2e9c820605b6e1
Anaïs Kajjaj <100074659+Anaderis@users.noreply.github.com>
2024-04-10 11:40:46 +0200

    add

--- templates/header.php
+++ templates/header.php
@@ -20,7 +20,8 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!-- Modification du chemin d'accès vers le fichier css -->
+    <link href="./assets/css/theme.css" rel="stylesheet">
 
 </head>
 <body>
