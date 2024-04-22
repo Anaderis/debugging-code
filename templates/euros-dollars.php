d9e3ce0c7b81083dd0288770649677b8b5af7a2c
Nathan Le Roux <152197824+NathanLeRoux29@users.noreply.github.com>
2024-04-22 15:02:24 +0200

    22/04/2024 Nathan (Devise)

--- templates/euros-dollars.php
+++ templates/euros-dollars.php
@@ -6,7 +6,7 @@ template('header', array(
 
     <!-- ======= About Section ======= -->
     <section id="homepage" class="homepage">
-        <div class="container">
+        <div class="container w-75">
             <div class="section-title">
                 <h2>Convertisseur de devise</h2>
             </div>
@@ -16,10 +16,10 @@ template('header', array(
                 <fieldset class="col-12 mt-4">
                     <legend>Euro vers dollar américain</legend>
                     <form action="" method="post" name="euros-dollars">
-                        <div class="form-group row">
-                            <div class="col">
+                        <div class="form-group row my-3">
+                            <div class="col-4">
                                 <label for="EUR" aria-hidden="true" hidden>Euros</label>
-                                <div class="input-group">
+                                <div class="input-group w-100">
                                     <input id="EUR" name="EUR" type="text" class="form-control" required>
                                     <div class="input-group-append">
                                         <div class="input-group-text">€</div>
@@ -27,13 +27,13 @@ template('header', array(
                                 </div>
                             </div>
 
-                            <div class="d-inline-flex align-items-center ">
+                            <div class="d-inline-flex align-items-center col-2">
                                 <span class="ver">vaut actuellement</span>
                             </div>
 
-                            <div class="col">
+                            <div class="col-4">
                                 <label for="USD" aria-hidden="true" hidden>Dollars</label>
-                                <div class="input-group">
+                                <div class="input-group w-100">
                                     <input id="USD" name="USD" type="text" class="form-control" disabled>
                                     <div class="input-group-append">
                                         <div class="input-group-text">$</div>
@@ -41,7 +41,7 @@ template('header', array(
                                 </div>
                             </div>
                             <div class="col-2">
-                                <button name="submit" type="submit" class="btn btn-primary btn-block">Calculer</button>
+                                <button name="submit" type="submit" class="btn btn-primary btn-block w-100">Calculer</button>
                             </div>
 
                             <!--https://fr.calcuworld.com/calculs-mathematiques/calculatrice-pourcentage/-->
@@ -49,11 +49,11 @@ template('header', array(
                     </form>
                 </fieldset>
 
-                <fieldset class="col-12 mt-4">
+                <fieldset class="col-12 mt-5">
                     <legend>Dollar américain vers euro</legend>
                     <form action="" method="post" name="euros-dollars">
-                        <div class="form-group row">
-                            <div class="col">
+                        <div class="form-group row my-3">
+                            <div class="col-4">
                                 <label for="USD" aria-hidden="true" hidden>Dollars</label>
                                 <div class="input-group">
                                     <input id="USD" name="USD" type="text" class="form-control" required>
@@ -63,11 +63,11 @@ template('header', array(
                                 </div>
                             </div>
 
-                            <div class="d-inline-flex align-items-center ">
+                            <div class="d-inline-flex align-items-center col-2">
                                 <span class="ver">vaut actuellement</span>
                             </div>
 
-                            <div class="col">
+                            <div class="col-4">
                                 <label for="EUR" aria-hidden="true" hidden>Euros</label>
                                 <div class="input-group">
                                     <input id="EUR" name="EUR" type="text" class="form-control" disabled>
@@ -78,7 +78,7 @@ template('header', array(
                             </div>
 
                             <div class="col-2">
-                                <button name="submit" type="submit" class="btn btn-primary btn-block">Calculer</button>
+                                <button name="submit" type="submit" class="btn btn-primary btn-block w-100">Calculer</button>
                             </div>
                         </div>
                     </form>
